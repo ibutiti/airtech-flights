ebe674092c6deb510e9719451ccf2507bba72b47
Allan Ibutiti <allan1mwangi@gmail.com>
2019-05-16 12:18:42 +0300

    add django scaffolding

---
+++ airtech_flights/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for airtech_flights project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airtech_flights.settings')
+
+application = get_wsgi_application()
