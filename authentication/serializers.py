e9d43ca9ff6be4c0d1452ab482b2c1abed579b3d
Allan Ibutiti <allan1mwangi@gmail.com>
2019-05-29 12:49:34 +0300

    add user signup serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,4 +1,39 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+
+from authentication.models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserBaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = User
+        fields = ('email',)
+
+    def validate(self, data):
+        '''Validate sign up data.'''
+
+        for field in self.get_fields().keys():
+            if not data.get(field):
+                raise serializers.ValidationError(f'{field} is required')
+        super().validate(data)
+
+        return data
+
+
+class UserSignUpSerializer(UserBaseSerializer):
+    '''Serializer for user sign up operation'''
+
+    class Meta:
+
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+
+        return User.objects.create_user(
+            email=validated_data.get('email'),
+            password=validated_data.get('password'),
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name')
+        )
