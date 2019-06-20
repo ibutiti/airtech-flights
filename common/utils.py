c7f3fafdaff9539d8c694ad05e66f03325f3e137
Allan Ibutiti <25291950+ibutiti@users.noreply.github.com>
2019-06-20 12:26:31 +0300

    add tickets model

---
+++ common/utils.py
@@ -0,0 +1,13 @@
+'''
+Common utilities
+'''
+from enum import Enum
+
+
+class ChoiceEnum(Enum):
+    '''
+    Enum with choices attribute for use in models
+    '''
+    @classmethod
+    def choices(cls):
+        return tuple((choice.name, choice.value) for choice in cls)
--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,36 @@
+'''
+Flight ticket model
+'''
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+from common.utils import ChoiceEnum
+
+
+class TicketStatus(ChoiceEnum):
+    '''Ticket status choices'''
+    reservation = 'RESERVATION'
+    paid = 'PAID'
+
+
+class Ticket(BaseModel):
+    '''Model for flight tickets'''
+    flight = models.ForeignKey(
+        'flights.flight',
+        null=False,
+        blank=False,
+        on_delete=models.DO_NOTHING
+    )
+    user = models.ForeignKey(
+        'authentication.user',
+        null=False,
+        blank=False,
+        on_delete=models.DO_NOTHING
+    )
+    status = models.CharField(
+        default=TicketStatus.reservation,
+        choices=TicketStatus.choices(),
+        null=False,
+        blank=False,
+        max_length=16
+    )
