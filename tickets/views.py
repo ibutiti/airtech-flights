2482f8013a03ad7faca9d9d7e4169ebd5f85fd05
Allan Ibutiti <25291950+ibutiti@users.noreply.github.com>
2019-06-24 21:45:07 +0300

    add ticket status endpoint

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,11 @@
 '''
 Ticket views
 '''
-from rest_framework import exceptions
+from rest_framework import exceptions, status
+from rest_framework import serializers
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from common.views import CreateRetrieveDestroyViewset
 from tickets.models import Ticket
@@ -30,3 +34,37 @@ class TicketViewset(CreateRetrieveDestroyViewset):
         context['user'] = self.request.user
 
         return context
+
+
+class TicketStatusView(APIView):
+    '''View for admins to change the status of a ticket'''
+    permission_classes = (IsAdminUser,)
+
+    def post(self, request, format=None):
+        '''Change status of a ticket'''
+        try:
+            ticket = Ticket.objects.get(pk=request.data.get('ticket_id'))
+        except Ticket.DoesNotExist:
+            raise serializers.ValidationError('Ticket does not exist')
+
+        ticket_status = request.data.get('ticket_status')
+
+        if not ticket_status:
+            raise serializers.ValidationError('Ticket status is required')
+
+        ticket.status = ticket_status
+        ticket.save()
+
+        # notify user of ticket status change
+        ticket.send_ticket_to_user(message_type='PAID')
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    def get(self, request):
+        '''Get all reserved tickets'''
+        tickets = Ticket.objects.filter(status='RESERVATION').all()
+
+        serializer = TicketSerializer(tickets, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
